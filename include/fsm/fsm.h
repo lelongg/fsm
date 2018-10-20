94a3091475c20ecf05c35066ed2bbe46f8a3221f
Gérald Lelong <gerald.lelong@easymov.fr>
2018-10-20 16:28:26 +0200

    prepare for release

--- include/fsm/fsm.h
+++ include/fsm/fsm.h
@@ -8,57 +8,6 @@
 #include <type_traits>
 #include <vector>
 
-template <typename T, T... I>
-struct integer_sequence
-{
-    static_assert(std::is_integral<T>::value, "Integral type");
-
-    using type = T;
-
-    static constexpr T size = sizeof...(I);
-
-    /// Generate an integer_sequence with an additional element.
-    template <T N>
-    using append = integer_sequence<T, I..., N>;
-
-    using next = append<size>;
-};
-
-template <typename T, T... I>
-constexpr T integer_sequence<T, I...>::size;
-
-template <std::size_t... I>
-using index_sequence = integer_sequence<std::size_t, I...>;
-
-namespace detail {
-// Metafunction that generates an integer_sequence of T containing [0, N)
-template <typename T, T Nt, std::size_t N>
-struct iota
-{
-    static_assert(Nt >= 0, "N cannot be negative");
-
-    using type = typename iota<T, Nt - 1, N - 1>::type::next;
-};
-
-// Terminal case of the recursive metafunction.
-template <typename T, T Nt>
-struct iota<T, Nt, 0ul>
-{
-    using type = integer_sequence<T>;
-};
-} // namespace detail
-
-// make_integer_sequence<T, N> is an alias for integer_sequence<T, 0,...N-1>
-template <typename T, T N>
-using make_integer_sequence = typename detail::iota<T, N, N>::type;
-
-template <int N>
-using make_index_sequence = make_integer_sequence<std::size_t, N>;
-
-// index_sequence_for<A, B, C> is an alias for index_sequence<0, 1, 2>
-template <typename... Args>
-using index_sequence_for = make_index_sequence<sizeof...(Args)>;
-
 namespace FSM {
 
 template <typename EventType>
@@ -75,31 +24,11 @@ struct Transition : VirtualTransition<EventType>
     static const StateType to_state = ToState;
 };
 
-// template <class Function, typename Index, std::underlying_type_t<Index>...
-// Indices> constexpr auto make_array_helper(
-//     Function f, Index, integer_sequence<std::underlying_type_t<Index>,
-//     Indices...>)
-//     -> std::array<
-//         typename
-//         std::result_of<Function(std::underlying_type_t<Index>)>::type,
-//         sizeof...(Indices)>
-// {
-//     return {{f(Indices)...}};
-// }
-//
-// template <typename Index, std::underlying_type_t<Index> N, typename Function>
-// constexpr auto make_array(Function f)
-//     -> std::array<typename
-//     std::result_of<Function(std::underlying_type_t<Index>)>::type, N>
-// {
-//     using underlaying = std::underlying_type_t<Index>;
-//     return make_array_helper(f, Index(), make_integer_sequence<underlaying,
-//     N>{});
-// }
-
 template <class Function, std::size_t... Indices>
-constexpr auto make_array_helper(Function f, index_sequence<Indices...>)
-    -> std::array<typename std::result_of<Function(std::size_t)>::type, sizeof...(Indices)>
+constexpr auto make_array_helper(Function f, std::index_sequence<Indices...>)
+    -> std::array<
+        typename std::result_of<Function(std::size_t)>::type,
+        sizeof...(Indices)>
 {
     return {{f(Indices)...}};
 }
@@ -108,7 +37,7 @@ template <int N, class Function>
 constexpr auto make_array(Function f)
     -> std::array<typename std::result_of<Function(std::size_t)>::type, N>
 {
-    return make_array_helper(f, make_index_sequence<N>{});
+    return make_array_helper(f, std::make_index_sequence<N>{});
 }
 
 template <typename StateType, typename EventType, typename... Transitions>
@@ -120,48 +49,39 @@ constexpr std::underlying_type_t<StateType> MaxState()
 template <typename StateType, typename EventType, typename... Transitions>
 struct TransitionHelper;
 
-template <typename StateType, typename... Transitions>
-struct TransitionTable;
-
-template <typename StateType, typename EventType, typename Transition, typename... Transitions>
-struct TransitionTable<StateType, EventType, Transition, Transitions...>
-{
-    using TransitionHelper_ =
-        TransitionHelper<StateType, EventType, Transition, Transitions...>;
-
-    static constexpr auto max_state =
-        MaxState<StateType, EventType, Transitions...>();
-
-//     static constexpr auto transitions =
-//         make_array<std::size_t(max_state + 1)>(TransitionHelper_::getToState);
-};
-
-template <typename StateType, typename EventType>
-struct TransitionTable<StateType, EventType>
-{
-    using TransitionHelper_ = TransitionHelper<StateType, EventType>;
-
-    static constexpr auto max_state = MaxState<StateType, EventType>();
-
-//     static constexpr auto transitions =
-//         make_array<std::size_t(max_state + 1)>(TransitionHelper_::getToState);
-};
-
 namespace detail {
-template <typename StateType, typename EventType, typename Transition, typename... Transitions>
-std::array<StateType, std::size_t(MaxState<StateType, EventType, Transitions...>() + 1)> transitions() {
+template <
+    typename StateType,
+    typename EventType,
+    typename Transition,
+    typename... Transitions>
+std::array<StateType, std::size_t(MaxState<StateType, EventType, Transitions...>() + 1)> transitions()
+{
     using TransitionHelper_ =
         TransitionHelper<StateType, EventType, Transition, Transitions...>;
-    static std::array<StateType, std::size_t(MaxState<StateType, EventType, Transitions...>() + 1)> transition_array =
-        make_array<std::size_t(MaxState<StateType, EventType, Transitions...>() + 1)>(TransitionHelper_::getToState);
+    static std::array<
+        StateType,
+        std::size_t(MaxState<StateType, EventType, Transitions...>() + 1)>
+        transition_array = make_array<std::size_t(
+            MaxState<StateType, EventType, Transitions...>() + 1)>(
+            TransitionHelper_::getToState);
     return transition_array;
 }
-}
+} // namespace detail
 
-template <typename StateType, typename EventType, typename Transition, typename... Transitions>
-static const auto& transitions = detail::transitions<StateType, EventType, Transition, Transitions...>();
+template <
+    typename StateType,
+    typename EventType,
+    typename Transition,
+    typename... Transitions>
+static const auto& transitions =
+    detail::transitions<StateType, EventType, Transition, Transitions...>();
 
-template <typename StateType, typename EventType, typename Transition, typename... Transitions>
+template <
+    typename StateType,
+    typename EventType,
+    typename Transition,
+    typename... Transitions>
 struct TransitionHelper<StateType, EventType, Transition, Transitions...>
 {
     static constexpr StateType getToState(const std::size_t from_state)
@@ -173,8 +93,8 @@ struct TransitionHelper<StateType, EventType, Transition, Transitions...>
         }
         else
         {
-            return TransitionHelper<StateType, EventType, Transitions...>::getToState(
-                from_state);
+            return TransitionHelper<
+                StateType, EventType, Transitions...>::getToState(from_state);
         }
     }
 };
@@ -191,9 +111,6 @@ struct TransitionHelper<StateType, EventType>
 template <typename StateType, typename... Transitions>
 struct FSM
 {
-    template <typename Event>
-    using TransitionList = TransitionTable<StateType, Event, Transitions...>;
-
     template <StateType FromState, typename EventType, StateType ToState>
     using Transition = Transition<StateType, FromState, EventType, ToState>;
 
@@ -205,9 +122,11 @@ struct FSM
         const auto state_as_underlying_type =
             static_cast<std::underlying_type_t<StateType>>(state_);
 
-        if (TransitionList<Event>::max_state >= state_as_underlying_type)
+        if (MaxState<StateType, Event, Transitions...>()
+            >= state_as_underlying_type)
         {
-            state_ = transitions<StateType, Event, Transitions...>[state_as_underlying_type];
+            state_ = transitions<
+                StateType, Event, Transitions...>[state_as_underlying_type];
         }
 
         return state_;
--- tests/fsm_tests.cpp
+++ tests/fsm_tests.cpp
@@ -2,42 +2,6 @@
 #include <fsm/fsm.h>
 #include <iostream>
 
-struct string_view
-{
-    char const* data;
-    std::size_t size;
-};
-
-inline std::ostream& operator<<(std::ostream& o, string_view const& s)
-{
-    return o.write(s.data, s.size);
-}
-
-template <class T>
-constexpr string_view get_name()
-{
-    char const* p = __PRETTY_FUNCTION__;
-    while (*p++ != '=')
-        ;
-    for (; *p == ' '; ++p)
-        ;
-    char const* p2 = p;
-    int count = 1;
-    for (;; ++p2)
-    {
-        switch (*p2)
-        {
-            case '[':
-                ++count;
-                break;
-            case ']':
-                --count;
-                if (!count) return {p, std::size_t(p2 - p)};
-        }
-    }
-    return {};
-}
-
 struct Event {};
 struct Event0 {};
 struct Event1 {};
@@ -50,42 +14,10 @@ enum State
     Stopped
 };
 
-TEST_CASE("Transition creation")
-{
-    using FSM::Transition;
-
-    Transition<State, Started, Event0, Stopped> transition;
-}
-
-TEST_CASE("Transition table creation")
-{
-//     using FSM::TransitionTable;
-//     using FSM::Transition;
-//
-//     using CustomTransitionTable = TransitionTable<
-//         State, Event1, Transition<State, NotInitialized, Event0, Initialized>,
-//         Transition<State, Initialized, Event0, Started>,
-//         Transition<State, Started, Event1, Stopped>>;
-
-//     auto a =
-//         CustomTransitionTable::transitions[int(Started)];
-//     REQUIRE(int(a) == 3);
-}
-
-template <typename TransitionList>
-void printTransitionList(const std::string& prefix)
-{
-//     for (size_t i = 0; i < TransitionList::transitions.size(); ++i)
-//     {
-//         std::cout << prefix << ": " << i << " -> "
-//                   << int(TransitionList::transitions[i]) << std::endl;
-//     }
-}
-
 template <State FromState, typename EventType, State ToState>
 using Transition = FSM::Transition<State, FromState, EventType, ToState>;
 
-TEST_CASE("FSM creation")
+TEST_CASE("FSM")
 {
     using FSM::FSM;
 
